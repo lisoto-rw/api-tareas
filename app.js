c2b3606ea9d924f77524a4a52fe3e2302364b12d
Lucas <lisoto@udc.edu.ar>
2026-06-29 19:54:07 -0300

    fix: se arreglo error en app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const tareasRoutes = require('./routes/routesTareas');//importacion de rutas par
 app.use(express.json());
 
 
-// se define /api como prefijo global para todas las rutas
+// se define /api como prefijo global para todas las rutasss
 app.use('/tareas', tareasRoutes);      
 
 
